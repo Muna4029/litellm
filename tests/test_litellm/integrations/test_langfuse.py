e304482c7a549ea3face411e8da764e4414f39d5
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-24 18:47:37 -0400

    Muna4029__diff__id_393

--- tests/test_litellm/integrations/test_langfuse.py
+++ tests/test_litellm/integrations/test_langfuse.py
@@ -13,6 +13,7 @@ import pytest
 
 import litellm
 from litellm.integrations.langfuse.langfuse import LangFuseLogger
+import litellm.integrations.langfuse.langfuse as langfuse_module
 
 
 def test_max_langfuse_clients_limit():
@@ -20,9 +21,8 @@ def test_max_langfuse_clients_limit():
     Test that the max langfuse clients limit is respected when initializing multiple clients
     """
     # Set max clients to 2 for testing
-    with patch(
-        "litellm.integrations.langfuse.langfuse.MAX_LANGFUSE_INITIALIZED_CLIENTS", 2
-    ):
+    # Patch the constant in the langfuse module's namespace where it's used
+    with patch.object(langfuse_module, "MAX_LANGFUSE_INITIALIZED_CLIENTS", 2):
         # Reset the counter
         litellm.initialized_langfuse_clients = 0
 
